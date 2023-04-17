b74dc906a1623ec790a520405dd304f4c964961a
MoonshotsLaptop <gamoonshots@gmail.com>
2023-04-17 16:17:16 -0700

    Update Camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Camera.java
@@ -12,6 +12,12 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -30,61 +36,90 @@ public class Camera {
     };
     public static final String VUFORIA_KEY = "AS6a0rf/////AAABmQTWiptCrE+LufdlUzBT8weOkDKTan22xYq7kRmbpkAd2B1wy+uBaVuTdp4ngclG4NG6WQ8+8+nnRd+v6OB5Gzm+jMMh02iC+WrML6/2ArgWlM1vh43nEyfKOaOyJ4uZYqKMNAEcXqNLKK2+PdtmQQgiwGhna/VKV/Qdkhwsxt6w+4VGETJJwxT8k+tXTal2DGF5Sr9c69Lz0O0drCDZ2+ZUtuhOn1X+dkVoGxAoqSh/sYiqssxEtfGaf551TQytAXNBpbMgYXNGRSR6WAke2lVC4BxEowhiacPiZDLOZgVrHPc0bJbtN2kIF3OWk/FHj3tuHQ6seHZR4cU/6S7AeP3PaBwnYKbFvg8svUAy3vxD";
 
-    private VuforiaLocalizer vuforia;
     private Telemetry telemetry;
     private TFObjectDetector tfod;
+    private OpenCvWebcam webcam;
 
+    private CVPipeline pipeline;
+
+    public CVPipeline getPipeline() {
+        return pipeline;
+    }
+
+    public int getFrameCount() {
+        return webcam.getFrameCount();
+    }
+
+    public float getFps() {
+        return webcam.getFps();
+    }
+
+    public float getTotalFrameTime() {
+        return webcam.getTotalFrameTimeMs();
+    }
+
+    public float getPipelineTime() {
+        return webcam.getPipelineTimeMs();
+    }
+
+    public float getOverheadTime() {
+        return webcam.getOverheadTimeMs();
+    }
+
+    public float getCurrentPipelineMaxFps() {
+        return webcam.getCurrentPipelineMaxFps();
+    }
 
     public Camera(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
 
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(
+                hardwareMap.get(WebcamName.class, "Webcam 1"),
                 hardwareMap.appContext.getResources().getIdentifier(
                         "cameraMonitorViewId",
                         "id",
                         hardwareMap.appContext.getPackageName()
                 )
         );
+        pipeline = new CVPipeline();
 
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        try {
-            parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
-            telemetry.addData("Camera", "Found");
-        } catch(Exception e) {
-            telemetry.addData("Camera", "Not found");
-        }
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        FtcDashboard.getInstance().startCameraStream(vuforia, 30);
-
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters();
-        tfodParameters.minResultConfidence = 0.75f;
-        tfodParameters.isModelTensorFlow2 = true;
-        tfodParameters.inputSize = 300;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-
-        // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
-        // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
-        try {
-            tfod.loadModelFromFile(MODEL_FILE, LABELS);
-            telemetry.addData("TFOD", "Loaded model from file");
-        } catch(Exception e) {
-            try {
-                tfod.loadModelFromAsset(MODEL_ASSET, LABELS);
-                telemetry.addData("TFOD", "Loaded model from asset");
-            } catch(Exception a) {
-                telemetry.addData("TFOD", "No model loaded");
+        webcam.setMillisecondsPermissionTimeout(5000);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.setPipeline(pipeline);
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
-        }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+                telemetry.addData("Camera", String.format("Failed to open webcam with code %x", errorCode));
+            }
+        });
     }
 
-    public final List<Recognition> getRecognitions() {
-        List<Recognition> rList = tfod.getRecognitions();
-        //if(rList == null) {
-         //   rList = Collections.emptyList();
-        //}
-        return rList;
+    private class CVPipeline extends OpenCvPipeline {
+        private boolean viewportPaused = false;
+        @Override
+        public Mat processFrame(Mat mat) {
+            return mat;
+        }
+
+        @Override
+        public void onViewportTapped() {
+            viewportPaused = !viewportPaused;
+
+            if (viewportPaused) {
+                webcam.pauseViewport();
+            } else {
+                webcam.resumeViewport();
+            }
+        }
     }
 }
