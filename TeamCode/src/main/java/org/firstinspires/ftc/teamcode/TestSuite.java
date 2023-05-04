d918507b3e88bafb40c60203c62669993a50457f
MoonshotsLaptop <gamoonshots@gmail.com>
2023-05-04 09:40:58 -0700

    Add simulator and opencv source code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSuite.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSuite.java
@@ -6,10 +6,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.teamcode.vision.Camera;
-
-import java.util.List;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
 
 @TeleOp(name = "Test Suite")
 public class TestSuite extends LinearOpMode {
@@ -21,8 +22,7 @@ public class TestSuite extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        camera = new Camera(hardwareMap, telemetry);
-
+        camera = new Camera( hardwareMap, telemetry);
 
         while(opModeInInit()) {
             telemetry.addData("UPS", 1 / runtime.seconds());
@@ -33,6 +33,7 @@ public class TestSuite extends LinearOpMode {
             telemetry.addData("CV Pipeline Time", String.format("%.2f ms", camera.getPipelineTime()));
             telemetry.addData("CV Overhead Time", String.format("%.2f ms", camera.getOverheadTime()));
             telemetry.addData("CV Theoretical Max FPS", camera.getCurrentPipelineMaxFps());
+            telemetry.addData("CV Viewport Enabled", camera.getPipeline().isViewportPaused());
             telemetry.update();
         }
     }
