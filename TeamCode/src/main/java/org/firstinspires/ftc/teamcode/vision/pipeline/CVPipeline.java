fa9577fe03a5bffe14d44dae054b054179bdc3a2
MoonshotsLaptop <gamoonshots@gmail.com>
2023-08-22 13:52:04 -0700

    Some Updates and old code replacement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/pipeline/CVPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/pipeline/CVPipeline.java
@@ -14,8 +14,42 @@ import java.util.List;
 
 public class CVPipeline extends OpenCvPipeline {
     private Telemetry telemetry;
-    private int conKSize = 11;
+    private int conKSize = 3;
+    private int cannyThresh1 = 100;
+    private int cannyThresh2 = 200;
 
+    public static class Cone {
+        public String color;
+        public int x;
+        public int y;
+        public int height;
+        public int width;
+
+        @Override
+        public String toString() {
+            return String.format("%s cone at (%f, %f)", color, x, y);
+        }
+    }
+
+    public static class Pole {
+        public enum Height {
+            HIGH,
+            MID,
+            LOW
+        }
+        Height height;
+
+        @Override
+        public String toString() {
+            return String.format("%s pole", height);
+        }
+    }
+
+    // Since opencv runs asynchronously to the robot,
+    // these MUST be declared volatile
+    // to avoid memory leaks and bad decisions
+    public volatile ArrayList<Cone> cones;
+    public volatile ArrayList<Pole> poles;
     public CVPipeline(Telemetry telemetry) {
         this.telemetry = telemetry;
     }
@@ -24,111 +58,81 @@ public class CVPipeline extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         Mat hsv = new Mat();
         Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
-        Mat holdingMat1 = new Mat();
-        Mat holdingMat2 = new Mat();
-        // region Split color channels
-        // Split the input image into two subimage channels
-        // If we are looking for blue, check red channel,
-        // since the cone red value is much lower than the floor's red value
-        // and vice versa
-        // coi: Column Index as according to the color mode, The input image's color mode is RGB
-        Mat redMask = new Mat();
-        Mat greenMask = new Mat();
-        Mat yellowMask = new Mat();
-        Mat blueMask = new Mat();
-        Core.extractChannel(input, blueMask, 0);
-        Core.extractChannel(input, greenMask, 1);
-        Core.extractChannel(input, redMask, 2);
-        // endregion
-
-        // region Find yellow color
-
-        Core.inRange(hsv, new Scalar(20, 30, 50), new Scalar(40, 255, 255), yellowMask);
-        // endregion
-        // region Use a convolution to blur the image
-        Mat redCon = new Mat();
-        Mat greenCon = new Mat();
-        Mat blueCon = new Mat();
-        Mat yellowCon = new Mat();
-
-        Imgproc.medianBlur(redMask, redCon, conKSize);
-        Imgproc.medianBlur(greenMask, greenCon, conKSize);
-        Imgproc.medianBlur(blueMask, blueCon, conKSize);
-        Imgproc.medianBlur(yellowMask, yellowCon, conKSize);
-        // endregion
-        // region Subtract out green channel noise from red and blue
+        if(hsv.empty()) {
+            return input;
+        }
+        // region draw an output image
+        Mat redAreas = findColor(hsv, 175, 15); // 160-180; 0-10
+        Mat yellowAreas = findColor(hsv, 30, 20); // 10-50
+        Mat blueAreas = findColor(hsv, 120, 15); // 105-135
         Mat redSub = new Mat();
         Mat blueSub = new Mat();
-
-        Core.subtract(redCon, greenCon, redSub);
-        Core.subtract(blueCon, greenCon, blueSub);
-        // endregion
-        // region Subtract out blue channel noise from yellow
         Mat yellowSub = new Mat();
 
-        Core.subtract(yellowCon, blueCon, yellowSub);
-        // endregion
-        // region Remove areas which are both red and blue (purple areas)
+        Core.subtract(redAreas, blueAreas, redSub);
+        Core.subtract(redSub, yellowAreas, redSub);
+        Core.subtract(blueAreas, redAreas, blueSub);
+        Core.subtract(blueSub, yellowAreas, blueSub);
+        Core.subtract(yellowAreas, redAreas, yellowSub);
+        Core.subtract(yellowSub, blueAreas, yellowSub);
+
+        Mat lines = new Mat();
         Mat redOutput = new Mat();
         Mat blueOutput = new Mat();
-        Mat coloredNoiseMap = new Mat();
-        Mat grayNoiseMap = new Mat();
+        Mat yellowOutput = new Mat();
         Mat rgbRed = new Mat();
         Mat rgbBlue = new Mat();
-        // Separate grayscale into 3 channels
-        Imgproc.cvtColor(redSub, rgbRed, Imgproc.COLOR_GRAY2RGB);
-        Imgproc.cvtColor(blueSub, rgbBlue, Imgproc.COLOR_GRAY2RGB);
-        // Create a noise map in purple
+        Mat rgbYellow = new Mat();
+        Imgproc.cvtColor(findLines(redSub, conKSize, cannyThresh1, cannyThresh2), rgbRed, Imgproc.COLOR_GRAY2RGB);
+        Imgproc.cvtColor(findLines(blueSub, conKSize, cannyThresh1, cannyThresh2), rgbBlue, Imgproc.COLOR_GRAY2RGB); //COLOR_GRAY2RGB
+        Imgproc.cvtColor(findLines(yellowSub, conKSize, cannyThresh1, cannyThresh2), rgbYellow, Imgproc.COLOR_GRAY2RGB); //COLOR_HSV2RGB
         Core.multiply(rgbRed, new Scalar(1, 0, 0), redOutput);
         Core.multiply(rgbBlue, new Scalar(0, 0, 1), blueOutput);
-        Core.add(blueOutput, redOutput, coloredNoiseMap);
-
-        Imgproc.cvtColor(coloredNoiseMap, grayNoiseMap, Imgproc.COLOR_RGB2GRAY);
-        Core.subtract(redSub, grayNoiseMap, redCon);
-        Core.subtract(blueSub, grayNoiseMap, blueCon);
-        // endregion
-        // region Find the image threshold areas
-        Mat redThresh = new Mat();
-        Mat blueThresh = new Mat();
-        Imgproc.threshold(redCon, redThresh, 35, 255, 0);
-        Imgproc.threshold(blueCon, blueThresh, 35, 255, 0);
-        // endregion
-        // region find contours
-        Mat redLines = new Mat();
-        Mat blueLines = new Mat();
-        List<MatOfPoint> redContours = new ArrayList<>();
-        List<MatOfPoint> blueContours = new ArrayList<>();
-        Mat redHierarchy = new Mat();
-        Mat blueHierarchy = new Mat();
-
-        Imgproc.Canny(redThresh, redLines, 100, 300);
-        Imgproc.Canny(blueThresh, blueLines, 100, 300);
-        Imgproc.findContours(redLines, redContours, redHierarchy, 1, 1);
-        Imgproc.findContours(blueLines, blueContours, blueHierarchy, 1, 1);
-
-        if(redContours.size() != 0) {
-            Rect biggest = Imgproc.boundingRect(redContours.get(0));
-
-            for(int i = 1; i < redContours.size(); i++) {
-                Rect compare = Imgproc.boundingRect(redContours.get(i));
-                if(compare.height * compare.width >= biggest.height * biggest.width) {
-                    biggest = compare;
-                }
-            }
-        }
-
-
-        // endregion
-        // region draw an output image
-        Mat output = new Mat();
-        Mat lines = new Mat();
-        Imgproc.cvtColor(redLines, rgbRed, Imgproc.COLOR_GRAY2RGB);
-        Imgproc.cvtColor(blueLines, rgbBlue, Imgproc.COLOR_GRAY2RGB);
-        Core.multiply(rgbRed, new Scalar(0, 0, 1), redOutput);
-        Core.multiply(rgbBlue, new Scalar(1, 0, 0), blueOutput);
-        Core.add(blueOutput, redOutput, lines);
+        Core.multiply(rgbYellow, new Scalar(1, 1, 0), yellowOutput);
+        Core.add(blueOutput, yellowOutput, lines);
+        Core.add(lines, redOutput, lines);
 
         return lines;
         // endregion
+    }
+
+    private Mat findColor(Mat hsv, int hue, int searchingRoom) {
+        Mat mask = new Mat();
+
+        Scalar lower1;
+        Scalar upper1;
+        Scalar lower2 = null;
+        Scalar upper2 = null;
+        if (hue > searchingRoom && hue < 180 - searchingRoom) {
+            lower1 = new Scalar(hue - searchingRoom, 70, 50);
+            upper1 = new Scalar(hue + searchingRoom, 255, 255);
+        } else if (hue <= searchingRoom) {
+            lower1 = new Scalar(1, 70, 50);
+            upper1 = new Scalar(hue + searchingRoom, 255, 255);
+            lower2 = new Scalar(180 - (hue - searchingRoom), 70, 50);
+            upper2 = new Scalar(180, 255, 255);
+        } else {
+            lower1 = new Scalar(hue - searchingRoom, 70, 50);
+            upper1 = new Scalar(180, 255, 255);
+            lower2 = new Scalar(1, 70, 50);
+            upper2 = new Scalar((hue + searchingRoom) % 180, 255, 255);
+        }
+
+        Core.inRange(hsv, lower1, upper1, mask);
+        if (lower2 != null && upper2 != null) {
+            Mat mask2 = new Mat();
+            Core.inRange(hsv, lower2, upper2, mask2);
+            Core.bitwise_or(mask, mask2, mask);
+        }
+
+        return mask;
+    }
+
+    private Mat findLines(Mat color, int ksize, int thresh1, int thresh2) {
+        Mat con = new Mat();
+        Mat lines = new Mat();
+        Imgproc.medianBlur(color, con, ksize);
+        Imgproc.Canny(con, lines, thresh1, thresh2);
+        return lines;
     }
 }
