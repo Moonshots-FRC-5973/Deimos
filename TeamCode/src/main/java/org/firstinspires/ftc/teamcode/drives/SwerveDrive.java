87554ba510bbb96607dc9f2ef6eb8f8d913d0eac
Calphast <gabrielnsouto100@gmail.com>
2023-08-25 17:10:39 -0400

    SwerveDrive Code Cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drives/SwerveDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drives/SwerveDrive.java
@@ -17,23 +17,29 @@ public class SwerveDrive extends Drivetrain {
     public final DcMotor rrMotor; // + Power = right wheel turning left
     private boolean fieldCentric = true;
 
+    //Encoder Methods
     public int getLeftEncodersDifference() {
         return llMotor.getCurrentPosition() - lrMotor.getCurrentPosition();
     }
+
+    public int getRightEncodersDifference() {
+        return rlMotor.getCurrentPosition() - rrMotor.getCurrentPosition();
+    }
+
+    //Wheel Methods 
     public double getLeftWheelAngle() {
         double angle = (360 * (getLeftEncodersDifference() % SWERVE_ENCODER_COUNTS_PER_REV) / SWERVE_ENCODER_COUNTS_PER_REV);
         if(fieldCentric) return angle + getIMU().getZAngle();
         return angle;
     }
-    public int getRightEncodersDifference() {
-        return rlMotor.getCurrentPosition() - rrMotor.getCurrentPosition();
-    }
+    
     public double getRightWheelAngle() {
         double angle = (360 * (getRightEncodersDifference() % SWERVE_ENCODER_COUNTS_PER_REV) / SWERVE_ENCODER_COUNTS_PER_REV);
         if (fieldCentric) return angle + getIMU().getZAngle();
         return angle;
     }
 
+    //Field Centric related Methods
     public void makeRobotCentric() {
         fieldCentric = false;
     }
@@ -73,6 +79,7 @@ public class SwerveDrive extends Drivetrain {
             if(Math.abs(leftWheelAngle) >= 90) {
                 multiplier = -1;
             }
+
             // multiplier flips rotation direction; Rest calculates how much rotation we need
             // https://www.geogebra.org/calculator/ucaxvmtw
             leftRotPower = SWERVE_WHEEL_ROT_MULTIPLIER * Math.sin(Math.toRadians(-leftWheelAngle));
@@ -128,6 +135,7 @@ public class SwerveDrive extends Drivetrain {
         if(Math.abs(leftAngleDiff) <= ANGLE_TOLERANCE) {
             leftAngleDiff = 0.0d;
         }
+
         if(Math.abs(rightAngleDiff) <= ANGLE_TOLERANCE) {
             rightAngleDiff = 0.0d;
         }
@@ -145,6 +153,7 @@ public class SwerveDrive extends Drivetrain {
         leftRotPower = multiplier * SWERVE_WHEEL_ROT_MULTIPLIER * Math.sin(Math.toRadians(leftAngleDiff));
         llPower += leftRotPower;
         lrPower -= leftRotPower;
+
         rightRotPower = multiplier * SWERVE_WHEEL_ROT_MULTIPLIER * Math.sin(Math.toRadians(rightAngleDiff));
         rlPower += rightRotPower;
         rrPower -= rightRotPower;
@@ -192,15 +201,19 @@ public class SwerveDrive extends Drivetrain {
         llMotor.setTargetPosition(
                 llMotor.getCurrentPosition() - (int)(distance * SWERVE_ENCODER_COUNTS_PER_INCH)
         );
+
         lrMotor.setTargetPosition(
                 lrMotor.getCurrentPosition() - (int)(distance * SWERVE_ENCODER_COUNTS_PER_INCH)
         );
+
         rlMotor.setTargetPosition(
                 rlMotor.getCurrentPosition() - (int)(distance * SWERVE_ENCODER_COUNTS_PER_INCH)
         );
+
         rrMotor.setTargetPosition(
                 rrMotor.getCurrentPosition() - (int)(distance * SWERVE_ENCODER_COUNTS_PER_INCH)
         );
+        
         llMotor.setPower(MOTOR_MAX_SPEED);
         lrMotor.setPower(MOTOR_MAX_SPEED);
         rlMotor.setPower(MOTOR_MAX_SPEED);
