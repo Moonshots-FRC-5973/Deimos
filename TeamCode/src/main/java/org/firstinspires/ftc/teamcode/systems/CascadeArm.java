06cab2fa0309d54427c641f2b64e8437ce873c5a
MoonshotsLaptop <gamoonshots@gmail.com>
2023-09-19 13:57:02 -0400

    Add CascadeArm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/CascadeArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/CascadeArm.java
@@ -1,2 +1,34 @@
-package org.firstinspires.ftc.teamcode.systems;public class CascadeArm {
+package org.firstinspires.ftc.teamcode.systems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class CascadeArm {
+    public static final int MAXIMUM = 500;
+    public static final int MINIMUM = 0;
+    public static final double POWER = 0.5;
+    protected DcMotor motor;
+    protected Telemetry telemetry;
+    public CascadeArm(HardwareMap hardwareMap, Telemetry telemetry) {
+        motor = hardwareMap.get(DcMotor.class, "testMotor");
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.telemetry = telemetry;
+
+    }
+    public void raiseArm(){
+        // RaiseArm
+        motor.setTargetPosition(MAXIMUM);
+        motor.setPower(POWER);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void lowerArm(){
+        // LowerArm
+        motor.setTargetPosition(MINIMUM);
+        motor.setPower(POWER);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
 }
